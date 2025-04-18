457777f80eebda42ce80105e7a361a774c5d23a5
”ns-develops” <test.tt9061030@gmail.com>
2025-04-18 10:32:59 +0200

    Initial Commit

---
+++ SwiftUI-Journal/SwiftUI_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JournalApp.swift
+//  SwiftUI-Journal
+//
+//  Created by Natalie S on 2025-04-18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-JournalTests/SwiftUI_JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JournalTests.swift
+//  SwiftUI-JournalTests
+//
+//  Created by Natalie S on 2025-04-18.
+//
+
+import Testing
+@testable import SwiftUI_Journal
+
+struct SwiftUI_JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
